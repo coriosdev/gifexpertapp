c7c4534671f6ed2cb3a48a582fe7e9a53cefa3e9
coriosdev <coriosdev@gmail.com>
2020-09-15 19:58:40 -0500

    pages

---
+++ docs/precache-manifest.bba87da6988367d266c52825052f0c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e41f9bd7b4c708b10f5dc4fd30d552",
+    "url": "https://coriosdev.github.io/gifexpertapp/index.html"
+  },
+  {
+    "revision": "e7769078497240c23806",
+    "url": "https://coriosdev.github.io/gifexpertapp/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "https://coriosdev.github.io/gifexpertapp/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://coriosdev.github.io/gifexpertapp/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7769078497240c23806",
+    "url": "https://coriosdev.github.io/gifexpertapp/static/js/main.1337e253.chunk.js"
+  },
+  {
+    "revision": "a0b32e4c963889bb9fe2",
+    "url": "https://coriosdev.github.io/gifexpertapp/static/js/runtime-main.d7190e1b.js"
+  }
+]);
